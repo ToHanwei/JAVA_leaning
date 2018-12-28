5107845daf924bc6ed5d848cde9a5b055870f1f4
ToHanwei <hanwei@shanghaitech.edu.cn>
2018-12-28 09:17:45 +0800

    2018-12-28_02

---
+++ hello/src/hello/hello.java
@@ -0,0 +1,21 @@
+package hello;
+
+import java.util.Scanner;
+
+public class hello {
+	
+	public static void main(String[] args) {
+		System.out.println("你好！");
+		Scanner in = new Scanner(System.in);
+//		System.out.println("echo:"+in.nextLine());
+//		final int amount=100;
+		int amount=100;
+		int price=0;
+		System.out.print("请输入票面：");
+		amount = in.nextInt();
+		System.out.print("请输入金额：");
+		price = in.nextInt();
+		System.out.println(amount+"-"+price+"="+(amount-price));
+	}
+
+}
