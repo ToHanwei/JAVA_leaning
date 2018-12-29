4b4e2c5cd36e2b7d0d6e7af639ea6e7627d713c2
DaWei <34996314+ToHanwei@users.noreply.github.com>
2018-12-29 09:29:26 +0800

    Add files via upload

---
+++ exercise_do_while.java
@@ -0,0 +1,17 @@
+package judge;
+
+public class exercise_do_while {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 0;
+		do
+		{
+			System.out.print("Value x = "+x);
+			x++;
+			System.out.print("\n");
+		} while (x<20);
+
+	}
+
+}
