c60e5f10910d15371a382659e174c26346e1c1e0
DaWei <34996314+ToHanwei@users.noreply.github.com>
2019-01-01 23:20:55 +0800

    Add files via upload

---
+++ class_for.java
@@ -0,0 +1,26 @@
+package judge;
+
+import java.util.Scanner;
+
+public class class_for {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+//		int i = 1;
+		int factor = 1;
+//		while (i <= n)
+//		{
+//			factor = factor * i;
+//			i = i +1;
+//		}
+		for (int i=1; i<=n; i=i+1)
+		{
+			factor = factor * i;
+		}
+		System.out.println(factor);
+
+	}
+
+}
