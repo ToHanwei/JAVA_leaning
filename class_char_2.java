e22e927962fced6f49c8d9b3078c3f063fa3307a
DaWei <34996314+ToHanwei@users.noreply.github.com>
2019-01-01 23:20:35 +0800

    Add files via upload

---
+++ class_char_2.java
@@ -0,0 +1,19 @@
+package judge;
+
+import java.util.Scanner;
+
+public class class_char_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in =new Scanner(System.in);
+		char c= 'A';
+		char d= (char)(c + ('a'-'A'));
+		System.out.println("abc\bd");
+		System.out.println("abc\td");
+		System.out.println("abc\nd");
+		System.out.println("abc\rd");
+
+	}
+
+}
