a2080b10e1af518972b83ae9ec670d667c5086e2
DaWei <34996314+ToHanwei@users.noreply.github.com>
2019-01-01 23:20:07 +0800

    2019_01_01

---
+++ class_char_1.java
@@ -0,0 +1,17 @@
+package judge;
+
+import java.util.Scanner;
+
+public class class_char_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in =new Scanner(System.in);
+		char c= 'A';
+		char d= (char)(c + ('a'-'A'));
+		System.out.print(d < 'º«');
+		
+
+	}
+
+}
